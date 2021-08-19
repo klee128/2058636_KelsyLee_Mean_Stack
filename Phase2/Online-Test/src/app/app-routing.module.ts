41cb32e823de8410691d0fb136448bad70dd8a2a
klee128 <klee128@ucsc.edu>
2021-08-19 00:10:25 -0700

    create angular project and start questions json file

---
+++ Phase2/Online-Test/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
+import { ResultComponent } from './result/result.component';
+import { ReviewComponent } from './review/review.component';
+
+const routes: Routes = [
+  { path: "quiz", component: QuizComponent },
+  { path: "result", component: ResultComponent },
+  { path: "review", component: ReviewComponent },
+  
+  { path: "", redirectTo: "quiz", pathMatch: "full" }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Phase2/Online-Test/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ReviewComponent } from './review/review.component';
+import { ResultComponent } from './result/result.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuizComponent,
+    ReviewComponent,
+    ResultComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
