ef5457e5ed50ecf327074ae58520018b2af4c26b
klee128 <klee128@ucsc.edu>
2021-08-24 11:30:38 -0700

    create a logger with some debugging statements

--- Phase3/Log-Records-And-Debugging/create_logs.js
+++ Phase3/Log-Records-And-Debugging/create_logs.js
@@ -1,19 +1,17 @@
 let readline = require('readline-sync');
 let fs = require('fs');
-
+debugger;
 
 
 // function that takes in first name, last name, and email from user input
 // 
 function createLog() {
     debugger;
-    let fName = readline.question("Enter your first name: ");
-    let lName = readline.question("Enter your last name: ");
-    let email = readline.questionEMail("Enter your email: ");
+    let fName = readline.question("Enter your first name: ", {defaultInput: 'John'});
+    let lName = readline.question("Enter your last name: ", {defaultInput: 'Smith'});
+    let email = readline.questionEMail("Enter your email: ", {defaultInput: 'jSmith@mail.com'});
     
     if (fs.existsSync('logs.json')) {
-        console.log("file exists");
-        debugger;
         // logs.json buffer -> string -> array
         let logsArray = JSON.parse(fs.readFileSync('logs.json').toString());
         
@@ -27,7 +25,6 @@ function createLog() {
         fs.writeFileSync("logs.json", JSON.stringify(logsArray));
 
     } else {
-        console.log("file doesn't exist");
         debugger;
         fs.writeFileSync("logs.json", JSON.stringify([{
             firstName: fName,
