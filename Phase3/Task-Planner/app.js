5b1974b33d959a75b0109beb9455e90dad3f1186
klee128 <klee128@ucsc.edu>
2021-08-28 18:14:04 -0700

    delete task and display table finished

--- Phase3/Task-Planner/app.js
+++ Phase3/Task-Planner/app.js
@@ -2,10 +2,6 @@ let http = require('http');  //create a server webpage
 let fs = require('fs');     //allow file manipulation
 let url = require('url')   //allow app to use url 
 
-let duplicateTIDhtml = `
-    <span style="color:red" > This task ID already exists. Try again. </span>
-`;
-let duplicateTIDbool = false;
 
 let addTaskHtml = `
     <form action="addTask">
@@ -25,13 +21,41 @@ let addTaskHtml = `
     </form>
 `;
 
+let duplicateTIDhtml = `
+    <span style="color:red" > This task ID already exists. Try again. </span>
+`;
+let duplicateTIDbool = false;
+
 let deleteTaskHtml = `
     <form action="deleteTask">
         <label>Task ID</label>
-        <input type="text" name="deletetid"/>
+        <input type="text" name="tid"/>
         <button type="submit">Delete Task</button>
     </form>
 `;
+let deleteFailMsg = `
+    <span style="color:red">Task with that ID does not exist. Try again. </span>
+`;
+let deleteFailBool = false;
+
+let displayTableHtml = `
+    <form action="displayTable">
+        <button type="submit">Display Tasks Table</button>
+    </form>
+`;
+let tableBegin = `
+    <table border=1>
+        <tr>
+            <th>Employee ID</th>
+            <th>Task ID</th>
+            <th>Task</th>
+            <th>Deadline</th>
+        <tr>
+`;
+let tableEnd = `
+    </table>
+`;
+
 
 let server = http.createServer((request, response) => {
     response.writeHead(200, { "content-type": "text/html" })
@@ -42,7 +66,11 @@ let server = http.createServer((request, response) => {
             response.writeHead(200, { "content-type": "text/html" })
             response.write("Placeholder");
             response.write(addTaskHtml);
+            response.write(`<hr />`);
             response.write(deleteTaskHtml);
+            response.write(`<hr />`);
+            response.write(displayTableHtml);
+
         }
         // if addTask form submitted
         else if (urlInfo.pathname == "/addTask") {
@@ -85,14 +113,65 @@ let server = http.createServer((request, response) => {
             if (duplicateTIDbool) response.write(duplicateTIDhtml);
             response.write(`<hr/>`);
             response.write(deleteTaskHtml);
+            response.write(`<hr />`);
+            response.write(displayTableHtml);
         }
         // if deleteTask form submitted
         else if (urlInfo.pathname == "/deleteTask"){
             response.writeHead(200, { "content-type": "text/html" })
+            if (fs.existsSync('tasks.json')) {
+                let taskArray = JSON.parse(fs.readFileSync('tasks.json').toString());
+                let taskIdx = taskArray.findIndex(t => t.tid == urlInfo.query.tid);
+                
+                if (taskIdx != -1) {
+                    deleteFailBool = false;
+                    console.log("task exists ind atabase. delete it" + taskIdx);
+                    taskArray.splice(taskIdx, 1);
+                    fs.writeFileSync('tasks.json', JSON.stringify(taskArray));
+                }
+                else {
+                    console.log("task not in database. error msg?" + taskIdx);
+                    deleteFailBool = true;
+                }
+            }
+
             response.write("delete task");
             response.write(addTaskHtml);
             response.write(`<hr/>`);
             response.write(deleteTaskHtml);
+            if (deleteFailBool) response.write(deleteFailMsg);
+            response.write(`<hr />`);
+            response.write(displayTableHtml);
+            
+        }
+        // if displayTable button is clicked 
+        else if (urlInfo.pathname == "/displayTable") {
+            console.log("display the table");
+            response.writeHead(200, { "content-type": "text/html" })
+            response.write("Placeholder");
+            response.write(addTaskHtml);
+            response.write(`<hr />`);
+            response.write(deleteTaskHtml);
+            response.write(`<hr />`);
+            response.write(displayTableHtml);
+            // code to iterate through tasks.json file and do tables stuff
+            let fullTable = tableBegin;
+            
+            if (fs.existsSync('tasks.json')) {
+                let taskArray = JSON.parse(fs.readFileSync('tasks.json').toString());
+                taskArray.forEach(task => {
+                    fullTable = fullTable + `
+                        <tr>
+                            <td>${task.eid}</td>
+                            <td>${task.tid}</td>
+                            <td>${task.task}</td>
+                            <td>${task.deadline}</td>
+                        </tr>
+                    `
+                });
+            }
+            fullTable = fullTable + tableEnd;
+            response.write(fullTable);
         }
         else {
             response.write("404 Page Not Found");
