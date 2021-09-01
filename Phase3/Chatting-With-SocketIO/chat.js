d7f6b5ec540e4b423217af3298e02728b5215938
klee128 <klee128@ucsc.edu>
2021-08-31 22:41:16 -0700

    styling changes

--- Phase3/Chatting-With-SocketIO/chat.js
+++ Phase3/Chatting-With-SocketIO/chat.js
@@ -1,23 +1,33 @@
-// javascript code for chat.html
-//test push
+// create a reference to server.js io
 var socket = io();
-// send data to server
-socket.emit("obj", "Hello Server, I am the Web Socket Client");
 
-// receive data from server
-socket.on("obj1", (msg) => {
-    console.log(msg);
-})
 
-// get user input from textfield
-// append to existing chatlog 
-// send to server
-function sendMsg(){
-    let msg = document.getElementById("urMsg").value;
-    console.log(msg);
+// on button click, send the user input to the server and get a response
+function sendMsg() {
+    // get user input from textfield and reset it
+    let userInput = document.getElementById("urMsg").value;
     document.getElementById("urMsg").value = "";
-    let msgNode = document.createTextNode(`You: ${msg}`);
-    let p = document.createElement("p");
-    p.appendChild(msgNode);
-    document.getElementById("chatlog").appendChild(p);
+
+    // create <p> tag with user's message and display it to chatlog
+    let msgNode = document.createTextNode(`You: ${userInput}`);
+    let userResponse = document.createElement("p");
+    userResponse.style.color = "#B52B40";
+    userResponse.appendChild(msgNode);
+    document.getElementById("chatlog").appendChild(userResponse);
+
+    // client sends user input to server
+    socket.emit("userMsg", userInput);
+
+    // client receives response from server
+    socket.once("servResponse", (servMsg) => {
+        // create <p> tag with server message
+        let servMsgNode = document.createTextNode(servMsg);
+        let serverResponse = document.createElement("p")
+        serverResponse.appendChild(servMsgNode);
+
+        // add server message to chat log and scroll to bottom
+        let chatlog = document.getElementById("chatlog");
+        chatlog.appendChild(serverResponse);
+        chatlog.scrollTop = chatlog.scrollHeight
+    })
 }
--- Phase3/Chatting-With-SocketIO/server.js
+++ Phase3/Chatting-With-SocketIO/server.js
@@ -1,14 +1,23 @@
 let express = require('express');
 let app = express();
-
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
+// array of server responses
+let responseArray = [
+    "That is a good question, let me find out for you.",
+    "I’m not sure, but let me find out for you.",
+    "I’m sorry, I don’t have the information on that. May I put you on hold for a few minutes?",
+    "Would you mind holding on for a few minutes while I am checking this with our administrator?",
+    "I’m sorry, we don’t have this feature at moment. We do intend to add it to our service and we can notify you when it has been done. Would you like to receive an email update?",
+    "I am really sorry this has happened. Let me see if I can find a solution to it.",
+    "I’m sorry you are not happy with our product/with your purchase. Let’s see what we can do to make things right."
+];
 
+// all three are needed to successfully run the app
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/chat.html");
 })
-// so chat.html can use chat.js functions
 app.get("/chat.js", (req, res) => {
     res.sendFile(__dirname + "/chat.js");
 })
@@ -19,15 +28,18 @@ app.get("/chat.css", (req, res) => {
 // when transferring data between server and client ... 
 // keep track of data by their keys 
 io.on("connection", (socket) => {
-    console.log("Client connected");
-    // socket.on("this must match whatever is in script in index.html")
-    // client -> server
-    socket.on("obj", (msg) => {
-        console.log(msg);
+    console.log("Client has connected to server.");
+
+    // server receive userMsg from client
+    socket.on("userMsg", (msg) => {
+        console.log("Client sent: " + msg);
+        let time = new Date().toTimeString().split(' ')[0];
+        let random = new Date().getSeconds() % responseArray.length;
+        // send a random server response back to client
+        socket.emit("servResponse", "Server (" + time + "): " + responseArray[random]);
     })
 
-    // server -> client
-    socket.emit("obj1", "Hello Client. You have connected to Server.")
 })
+
 // run server using http module not express module
 http.listen(9090, () => console.log("Server running on port 9090"));
